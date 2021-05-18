f833728417c8ae7f933b4c67c2608d8540bc86bc
akibkhan17 <wi20b098@technikum-wien.at>
2021-05-18 15:56:12 +0200

    Pom Angepasst und einen Test durchgeführt

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static String s= "Hallo ArchiverApp";
+
+    public static void main(String[] args){
+
+
+    }
+    public static String getMessage(){
+        return s;
+    }
+}
